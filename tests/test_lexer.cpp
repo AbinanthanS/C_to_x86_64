ebc22e96385a7e1f03ce6062096ebd9aec50d478
Abinanthan <s.a.abinanthan2005@gmail.com>
2026-03-12 23:47:24 +0530

    modified

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -2,11 +2,13 @@
 #include "lexer/lexer.hpp"
 #include <vector>
 
+using namespace c2x64;
+
 using c2x64::Lexer;
 using c2x64::TokenType;
 
-static std::vector<TokenType> types_of(Lexer& lex) {
-    std::vector<TokenType> out;
+static vector<TokenType> types_of(Lexer& lex) {
+    vector<TokenType> out;
     while (true) {
         auto t = lex.next();
         out.push_back(t.type);
@@ -19,7 +21,7 @@ TEST_CASE("lexer: keywords identifiers numbers") {
     Lexer lex("int main(){ return 42; }");
     auto got = types_of(lex);
 
-    std::vector<TokenType> expect = {
+    vector<TokenType> expect = {
         TokenType::KwInt,
         TokenType::Identifier, // main
         TokenType::LParen,
@@ -39,7 +41,7 @@ TEST_CASE("lexer: operators") {
     Lexer lex("a==b != c <= d >= e = f < g > h");
     auto got = types_of(lex);
 
-    std::vector<TokenType> expect = {
+    vector<TokenType> expect = {
         TokenType::Identifier,
         TokenType::EqEq,
         TokenType::Identifier,
@@ -65,7 +67,7 @@ TEST_CASE("lexer: line comments") {
     Lexer lex("int x; // comment\nreturn 0;");
     auto got = types_of(lex);
 
-    std::vector<TokenType> expect = {
+    vector<TokenType> expect = {
         TokenType::KwInt,
         TokenType::Identifier,
         TokenType::Semicolon,
